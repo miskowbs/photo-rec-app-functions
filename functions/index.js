572b55e924945d37440a272beaae99fe8cce784a
Bart Miskowiec <miskowbs@rose-hulman.edu>
2017-12-21 23:40:18 -0500

    completed annotateImage function

--- functions/index.js
+++ functions/index.js
@@ -6,15 +6,24 @@ const client = new vision.ImageAnnotatorClient();
 
 admin.initializeApp(functions.config().firebase);
 
-exports.annotateImage = functions.database.ref('/searches/{searchId}').onCreate(event => {
-    const object = event.data;
-    const filePath = object.name;
+exports.annotateImage = functions.database.ref('/searches/{searchId}/url').onCreate(event => {
+    const filePath = event.data.val();
+    console.log('url: ' + filePath);
 
-    client.labelDetection(filePath).then(results => {
+    
+    client
+    .labelDetection(filePath)
+    .then(results => {
         const labels = results[0].labelAnnotations;
+        console.log('Labels: ');
 
-        labels.foreach(label => {
-            event.data.ref.child('tags').push().set(label.description)
-        })
+        labels.forEach(label => {
+            console.log(label.description);
+            event.data.ref.parent.child('tags').push().set(label.description)
+        });
+        return labels;
     })
-})
+    .catch(err => {
+        console.error('ERROR:', err);
+    });
+});
